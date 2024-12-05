35f0ff24f5d7abe4d379c6f31b5f1d035f701b44
Frank <frantisek.sova@student.uibk.ac.at>
2024-12-05 14:40:45 +0100

    websocket implementation

--- src/edge.py
+++ src/edge.py
@@ -1,4 +1,23 @@
-import time
-while True:
-    print(f"hello world from {__file__} ")
-    time.sleep(1)
+import cv2
+import numpy as np
+import socketio
+import eventlet
+
+sio = socketio.Server()
+
+@sio.event
+def frame_event(sid, data):
+    frame_name = data['frame_name']
+    frame_data = data['data']
+    print(f'recieved {frame_name}')
+
+    np_arr = np.frombuffer(frame_data, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    output_path = f'camera_set\\video1\\{frame_name}'
+    cv2.imwrite(output_path, frame)
+
+app = socketio.WSGIApp(sio)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)
--- src/iot.py
+++ src/iot.py
@@ -1,5 +1,8 @@
 import os
 import cv2
+import socketio
+import time
+import sys
 
 video_path = 'camera_set\\video1.avi'
 video = cv2.VideoCapture(video_path)
@@ -9,11 +12,24 @@ if (video.isOpened() == False):
     print('Video file could not be opened')
 
 os.makedirs('camera_set\\video1', exist_ok=True)
-    
-i = 0
-while(video.isOpened()):
-    retval, frame = video.read()
-    if retval == True:
-        output_path = 'camera_set\\video1\\'+ f'frame{i}.jpg'
-        cv2.imwrite(output_path, frame)
-        i += 1
+
+
+sio = socketio.Client()
+sio.connect('http://localhost:5000')
+  
+
+def send_frames():
+    i = 1
+    while(video.isOpened()):
+        framerate = video.get(cv2.CAP_PROP_FPS)
+        retval, frame = video.read()
+        if retval == True:
+            retval, jpg = cv2.imencode('.jpg', frame)
+            data = jpg.tobytes()
+            frame_name = f"frame{i}.jpg"
+            sio.emit('frame_event', {'frame_name': frame_name, 'data': data})
+            i += 1
+            
+            time.sleep(1/framerate)
+
+send_frames()
