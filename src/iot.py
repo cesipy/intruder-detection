15edb5def4ad017106cfea083f5a41a32ff62798
Frank <frantisek.sova@student.uibk.ac.at>
2024-12-05 11:33:31 +0100

    iot parses video

--- src/iot.py
+++ src/iot.py
@@ -1,4 +1,19 @@
-import time
-while True:
-    print(f"hello world from {__file__} ")
-    time.sleep(1)
+import os
+import cv2
+
+video_path = 'camera_set\\video1.avi'
+video = cv2.VideoCapture(video_path)
+
+
+if (video.isOpened() == False):
+    print('Video file could not be opened')
+
+os.makedirs('camera_set\\video1', exist_ok=True)
+    
+i = 0
+while(video.isOpened()):
+    retval, frame = video.read()
+    if retval == True:
+        output_path = 'camera_set\\video1\\'+ f'frame{i}.jpg'
+        cv2.imwrite(output_path, frame)
+        i += 1
