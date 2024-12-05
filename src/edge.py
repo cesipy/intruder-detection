936c17cc7c0db43a872c1cebeee340908758a926
Frank <frantisek.sova@student.uibk.ac.at>
2024-12-05 20:20:52 +0100

    added comments

--- src/edge.py
+++ src/edge.py
@@ -5,9 +5,13 @@ import os
 import sys
 import eventlet
 
-sio = socketio.Server()
-
+# create folder for saving the frames (TESTING)
 dir_path = os.path.join('camera_set', 'video1')
+os.makedirs(dir_path, exist_ok=True)
+
+# create server and wrap it in wsgi server
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
 
 def trigger_alarm():
     sio.emit('alarm_event')
@@ -19,20 +23,19 @@ def frame_event(sid, data):
     print(f'recieved {frame_name}')
     sys.stdout.flush()
 
-    # alarm test
+    # alarm test (TESTING: TODO fix this)
     if (frame_name == 'frame100.jpg'):
         trigger_alarm()
 
+    # convert back to jpg
     np_arr = np.frombuffer(frame_data, np.uint8)
     frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    # TESTING
+    # save the frames (TESTING)
     #output_path = os.path.join(dir_path, frame_name)
     #cv2.imwrite(output_path, frame)
 
-app = socketio.WSGIApp(sio)
-
-os.makedirs(dir_path, exist_ok=True)
 
 if __name__ == '__main__':
+    # start server with its own loop to handle asynchronous connections
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
--- src/iot.py
+++ src/iot.py
@@ -3,13 +3,14 @@ import cv2
 import socketio
 import time
 
+# open the video file 
 video_path = os.path.join('camera_set', 'video1.avi')
 video = cv2.VideoCapture(video_path)
 
 if (video.isOpened() == False):
     print(f'Video file could not be opened on path {video_path}')
 
-
+# connect to the edge server 
 sio = socketio.Client()
 sio.connect('http://edge:5000')
   
@@ -17,7 +18,10 @@ sio.connect('http://edge:5000')
 def send_frames():
     i = 1
     while(video.isOpened()):
+        # extract frame rate from the video file
         framerate = video.get(cv2.CAP_PROP_FPS)
+
+        # parse the video into frames and send them to the server
         retval, frame = video.read()
         if retval == True:
             retval, jpg = cv2.imencode('.jpg', frame)
@@ -26,6 +30,7 @@ def send_frames():
             sio.emit('frame_event', {'frame_name': frame_name, 'data': data})
             i += 1
             
+            # simulate the real frame rate
             time.sleep(1/framerate)
 
 send_frames()
