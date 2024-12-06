94ee6f4348c46feff2b712c46305c220068cf519
Cedric Sillaber <cedric.sillaber@gmail.com>
2024-12-06 13:02:41 +0100

    edge to class

--- src/edge.py
+++ src/edge.py
@@ -4,55 +4,66 @@ import socketio
 import os
 import uvicorn
 
-sio = socketio.AsyncServer(async_mode="asgi")
+class EdgeServer:
+    def __init__(self):
+        self.sio = socketio.AsyncServer(async_mode="asgi")
+        self._setup_events()
+        
+    def _setup_events(self):
+        @self.sio.event
+        async def frame_event(sid, data):
+            await self._handle_frame_event(sid, data)
+        
+    async def trigger_alarm(self, ):
+        await self.sio.emit('alarm_event')
+
+    # TODO send further to cloud
+    async def process_frame(self, frame_data, frame_name):
+        np_arr = np.frombuffer(frame_data, np.uint8)
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        return
+        
+    def detect_intruder(self, frame_name: str, test_detection_freq: int=100): 
+        frame_name = frame_name.split("_")
+        frame_name = frame_name[1]
+        frame_name = frame_name.replace("frame", "")
+        frame_name = frame_name.replace(".jpg", "")
+        frame_number = int(frame_name)
+        
+        if frame_number % test_detection_freq == 0:
+            return True
+        
+
+    # TODO use sid to keep track of clients (to notify alarm what camera caught the intruder?)
+    async def _handle_frame_event(self, sid, data):
+        name = data['frame_name']
+        frame = data['data']
+        #print(f'recieved frame {name}')
+
+        # alarm trigger (TESTING)
+        if self.detect_intruder(name):
+            print(f"Intruder detected on video frame: {name}")
+            await self.trigger_alarm()
+            
+        # save frame to dir ./test-data
+        # with open(f"test-data/{name}", 'wb') as f:
+        #    f.write(frame)
+        
+        await self.process_frame(frame, name)
+        
 # dir_path = os.path.join('camera_set', 'frames') # (TESTING)
 
 # os.makedirs("test-data", exist_ok=True)
 
 
-async def trigger_alarm():
-    await sio.emit('alarm_event')
-
-# TODO send further to cloud
-async def process_frame(frame_data, frame_name):
-
-    np_arr = np.frombuffer(frame_data, np.uint8)
-    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-    return
-    
-def detect_intruder(frame_name: str, test_detection_freq: int=100): 
-    frame_name = frame_name.split("_")
-    frame_name = frame_name[1]
-    frame_name = frame_name.replace("frame", "")
-    frame_name = frame_name.replace(".jpg", "")
-    frame_number = int(frame_name)
-    
-    if frame_number % test_detection_freq == 0:
-        return True
-    
-
-# TODO use sid to keep track of clients (to notify alarm what camera caught the intruder?)
-@sio.event
-async def frame_event(sid, data):
-    name = data['frame_name']
-    frame = data['data']
-    #print(f'recieved frame {name}')
-
-    # alarm trigger (TESTING)
-    if detect_intruder(name):
-        print(f"Intruder detected on video frame: {name}")
-        await trigger_alarm()
-        
-    # save frame to dir ./test-data
-    # with open(f"test-data/{name}", 'wb') as f:
-    #    f.write(frame)
-    
-    await process_frame(frame, name)
 
 def main():
+    
+    edge = EdgeServer()
+    
     # wrap the server in asgi server and run it
-    app = socketio.ASGIApp(sio)
+    app = socketio.ASGIApp(edge.sio)
     uvicorn.run(app, host="0.0.0.0", port=5000)
 
     # create folder for saving the frames (TESTING)
